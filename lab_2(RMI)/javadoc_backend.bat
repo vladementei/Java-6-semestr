53f8bdf5f5b96c436ba5ea154c61acabaf6d87ac
Vlad Dementei <vladementei@gmail.com>
2020-04-24 08:06:17 +0300

    feat: javadoc scripts

---
+++ lab_2(RMI)/javadoc_backend.bat
@@ -0,0 +1,1 @@
+"C:\Program Files\Java\jdk-10.0.2\bin\javadoc.exe" -private -splitindex -use -author -version -link https://docs.oracle.com/javase/10/docs/api -html5 -d D:\IDEA\lab_2(RMI)\java_doc\backend @D:\IDEA\lab_2(RMI)\javadoc_backend_args.txt
---
+++ lab_2(RMI)/javadoc_frontend.bat
@@ -0,0 +1,1 @@
+"C:\Program Files\Java\jdk-10.0.2\bin\javadoc.exe" -private -splitindex -use -author -version -link https://docs.oracle.com/javase/10/docs/api -html5 -d D:\IDEA\lab_2(RMI)\java_doc\frontend @D:\IDEA\lab_2(RMI)\javadoc_frontend_args.txt
